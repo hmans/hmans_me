abc0f2fef9219c71d906e8a802516c73934f87f6
Hendrik Mans <hendrik@mans.de>
2017-01-10 15:22:20 +0100

    Proper page titles

--- site/_view.rb
+++ site/_view.rb
@@ -6,6 +6,12 @@ module SiteHelpers
   def blog_posts
     find('/posts').pages.sort_by {|p| p.data['date'] }.reverse
   end
+
+  def page_title
+    page.data["title"] ?
+      "#{page.data["title"]} &middot; Hendrik Mans" :
+      "Hendrik Mans, Web Person"
+  end
 end
 
 extend SiteHelpers
