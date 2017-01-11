bf9854d90c151b61cfdcc8acc2cd6153bac4640f
Hendrik Mans <hendrik@mans.de>
2017-01-11 14:12:52 +0100

    Experiment with _config.yml

--- site/_view.rb
+++ site/_view.rb
@@ -9,8 +9,16 @@ module SiteHelpers
 
   def page_title
     page.data["title"] ?
-      "#{page.data["title"]} &middot; Hendrik Mans" :
-      "Hendrik Mans, Web Person"
+      "#{page.data["title"]} &middot; #{config["site_title"]}" :
+      config["site_title"]
+  end
+
+  def config
+    config_node.data
+  end
+
+  def config_node
+    @config_node ||= find("/_config")
   end
 end
 
