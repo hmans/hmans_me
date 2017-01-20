fa7de7cb1178182bd2f627cb84276bb4c386c936
Hendrik Mans <hendrik@mans.de>
2017-01-20 16:54:05 +0100

    Simplify yearly archives

--- site/archive/_init.html.rb
+++ site/archive/_init.html.rb
@@ -1,24 +1,21 @@
 module PostArchive
   def self.extended(base)
+    base.load_years
     base.create_archive_pages
   end
 
-  def create_archive_pages
-    # Remove this node's extension because we're going to turn it
-    # into a folder, anyway. (It only has an extra HTML extension
-    # because otherwise, the processing wouldn't kick in!)
-    self.ext = nil
-
-    # Load all blog posts and group them by year.
+  def load_years
     all_posts = find("/posts").pages
     post_groups = all_posts.group_by { |p| p.data['date'].year }
+    data[:years] = post_groups.keys
+  end
 
+  def create_archive_pages
     # For each year, create a new node with an index page.
-    post_groups.each do |year, post|
-      page = sibling("_page").copy(year.to_s)
-      page.data['year'] = year
+    data[:years].each do |year|
+      find("_page").copy(year.to_s, year: year)
     end
   end
 end
 
-extend PostArchive
+parent.extend PostArchive
