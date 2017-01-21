7670a23117c4e8df799bd754f452fc0104dd64ce
Hendrik Mans <hendrik@mans.de>
2017-01-21 22:10:33 +0100

    Clean up archive initialization

--- site/archive/_init.rb
+++ site/archive/_init.rb
@@ -1,21 +1,12 @@
-module PostArchive
+extend_parent do
   def setup
-    load_years
-    create_archive_pages
-  end
-
-  def load_years
-    all_posts = find('/posts').posts
-    post_groups = all_posts.group_by { |p| p.data['date'].year }
-    data[:years] = post_groups.keys
-  end
-
-  def create_archive_pages
     # For each year, create a new node with an index page.
-    data[:years].each do |year|
+    years.each do |year|
       find("_page").copy(year.to_s, year: year)
     end
   end
-end
 
-parent.extend PostArchive
+  def years
+    @years ||= find('/posts').posts.map { |p| p.date.year }.uniq
+  end
+end
