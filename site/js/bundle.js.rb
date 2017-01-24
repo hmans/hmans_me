d33213c51443335fea086b2c196fb405a5ba1e89
Hendrik Mans <hendrik@mans.de>
2017-01-24 12:56:50 +0100

    More bundling experiments

--- site/js/bundle.js.rb
+++ site/js/bundle.js.rb
@@ -1,10 +1,33 @@
-def uglify(node)
-  Uglifier.new.compile(node.render)
+# My sorry excuse for an API:
+
+class JavaScriptBundle
+  attr_reader :nodes
+  
+  def initialize
+    @nodes = []
+  end
+
+  def <<(node)
+    if node
+      @nodes << node
+    end
+  end
+
+  def concatenate
+    @nodes.compact.map(&:render).join("\n")
+  end
+
+  def uglify
+    Uglifier.new.compile(concatenate)
+  end
 end
 
-output = ""
-output << uglify(find("./app.js"))
-output << uglify(find("./jquery-oembed.js"))
-output << uglify(find("./turbolinks.js"))
 
-output
+# Use that API
+bundle = JavaScriptBundle.new
+
+bundle << find("./app.js")
+bundle << find("./jquery-oembed.js")
+bundle << find("./turbolinks.js")
+
+bundle.uglify
