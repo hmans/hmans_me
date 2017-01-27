19a8c5e93e0b0757fbbebbf3c046bc57edc221f4
Hendrik Mans <hendrik@mans.de>
2017-01-27 13:52:42 +0100

    Use new syntax in other initializer, too

--- site/archive/_init.rb
+++ site/archive/_init.rb
@@ -1,5 +1,5 @@
 extend_parent do
-  on_setup do
+  on :setup do
     # For each year, create a new node with an index page.
     years.each do |year|
       find("_page").copy(year.to_s, year: year)
