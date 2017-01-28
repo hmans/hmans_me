26a5c74fdfc85254bbea47b7a564a9e65a807de9
Hendrik Mans <hendrik@mans.de>
2017-01-28 17:22:46 +0100

    Use caching in bundler

--- site/js/bundle.js.rb
+++ site/js/bundle.js.rb
@@ -2,6 +2,25 @@
 # This, of course, would be extracted into Flutterby or a plugin gem.
 # You will find the code that actually uses this further down the file.
 
+
+module Cache
+  def self.get(key)
+    store[key]
+  end
+
+  def self.set(key, value)
+    store[key] = value
+  end
+
+  def self.fetch(key)
+    get(key) || set(key, yield)
+  end
+
+  def self.store
+    $cache ||= {}
+  end
+end
+
 class JavaScriptBundle
   attr_reader :nodes
 
@@ -20,18 +39,30 @@ class JavaScriptBundle
   end
 
   def uglify
+    Cache.fetch(cache_key) do
+      uglify!
+    end
+  end
+
+  def cache_key
+    "bundle-#{newest_timestamp}"
+  end
+
+  def newest_timestamp
+    @nodes.map(&:timestamp).max
+  end
+
+  def uglify!
     Uglifier.compile(concatenate)
   end
 end
 
 
 # This is where we get to use the API:
-$output ||= begin
-  bundle = JavaScriptBundle.new
+bundle = JavaScriptBundle.new
 
-  find("./_bundle").children.each do |node|
-    bundle << node
-  end
-
-  bundle.uglify
+find("./_bundle").children.each do |node|
+  bundle << node
 end
+
+bundle.uglify
